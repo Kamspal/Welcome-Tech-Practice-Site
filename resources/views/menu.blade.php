911b5b251bd01e2fe313a57734cd9c6babd8b2ed
KamanChandraPal <kams.pal014@gmail.com>
2020-04-28 22:41:52 +0545

    added bg image and slider with navigation

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,2 +1,86 @@
-<body>
-    
+<header class="content">
+    <!-- Nav Bar -->
+ <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+  <a class="navbar-brand" href="#">Company Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Social elements -->
+  
+
+  <!-- end social elements -->
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Seo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sem</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-facebook"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-twitter"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-play"></i></a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+   <!-- end nav bar -->
+</header>
+
+<section class="style1">
+    <!-- Image slider -->
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="false">
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/bg-1.jpg" class="d-block w-100" alt="background image1">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>We are an IT <span class="lb" style="color: #02A1DD;;">consulting and training</span> firm</h5>
+        <p>Which specializes Cisco Systems VOIP Phone Systems, Network Installtation and troubleshoooting. We are experts in our field.</p>
+        <button type="button" class="color-lb">Learn More</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/bg-2.jpg" class="d-block w-100" alt="background image3">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>We are an IT <span class="lb" style="color: #02A1DD;;">consulting and training</span> firm</h5>
+        <p>Which specializes Cisco Systems VOIP Phone Systems, Network Installtation and troubleshoooting. We are experts in our field.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/bg-3.jpg" class="d-block w-100" alt="background image3">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>We are an IT <span class="lb" style="color: #02A1DD;;">consulting and training</span> firm</h5>
+        <p>Which specializes Cisco Systems VOIP Phone Systems, Network Installtation and troubleshoooting. We are experts in our field.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <!-- end Image Slider -->
+</section>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('menu');
+    return view('layouts/layout');
 });
